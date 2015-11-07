20b1500dc7be7c8865a8f4896476b8985cfb14d0
Shayan Farmani <sfarmani@ucsc.edu>
2015-11-06 17:59:35 -0800

    fixed some files?

--- controllers/default.py
+++ controllers/default.py
@@ -41,3 +41,20 @@ def index():
     return dict(email_list=email_list)
 
 
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
