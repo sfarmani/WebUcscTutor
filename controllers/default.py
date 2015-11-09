ef500da997c4a82fa12667985fa05929e1c25797
Shayan Farmani <sfarmani@ucsc.edu>
2015-11-08 17:00:12 -0800

    made it look pretty, can't figure out implementing parse

--- controllers/default.py
+++ controllers/default.py
@@ -2,59 +2,40 @@
 # this file is released under public domain and you can use without limitations
 
 import json
-import urllib2
+import httplib
+
+appId = "sm3IJPOksqi4vIIN99wmppWnGWFZ0lvsVLNQ9VuO"
+apiKey = "4udDwLuCkvJLR09ypSp1xsgKKVwBDmncRSRBd24K"
 
 
-def get_emails(url):
-    """This Python function is not a controller.  A function defines a controller in web2py
-    iff it does not take any arguments.  This function takes arguments, and so it's a normal
-    function you can call from your controllers."""
-    try:
-        r = urllib2.urlopen(url)
-        email_list = json.load(r)
-        r.close()
-    except Exception, e:
-        email_list = []
-        session.message = T('Error in contacting server')
-    return email_list
+def get_info(strng):
+    if strng == "get users":
+        connection = httplib.HTTPSConnection('api.parse.com', 443)
+        connection.connect()
+        connection.request('GET', '/1/users', '', {
+               "X-Parse-Application-Id": appId,
+               "X-Parse-REST-API-Key": apiKey
+             })
+        return json.loads(connection.getresponse().read())
+    # elif strng == "signup as tutor":
+    #     connection = httplib.HTTPSConnection('api.parse.com', 443)
+    #     connection.connect()
+    #     connection.request('POST', '/1/users', json.dumps({
+    #            "username": "cooldude6",
+    #            "password": "p_n7!-e8",
+    #            "phone": "415-392-0202"
+    #          }), {
+    #            "X-Parse-Application-Id": appId,
+    #            "X-Parse-REST-API-Key": apiKey,
+    #            "X-Parse-Revocable-Session": "1",
+    #            "Content-Type": "application/json"
+    #          })
+    #     return json.loads(connection.getresponse().read())
 
 
 def index():
-    """
-    Displays list of all emails.
-    """
-    # Gets some fresh set of emails each time.
-    email_list = get_emails('http://luca-teaching.appspot.com/fake_emails/default/get_emails')
-    # We store the list of emails in the session.
-    session.email_list = email_list
-    # At this point, email_list is a list of dictionaries of this format:
-    #     {
-    #         'id': 'randomstringid',
-    #         'from': 'sender@email.com',
-    #         'date': 'somedateinISOformat',
-    #         'subject': 'Your homework.',
-    #         'text': 'Text of email, with \n\n used to separate paragraphs',
-    #         'starred': True, # or False!
-    #         'read': False, # Whether you have read it or not, can be True or False
-    #     }
-    # And you need to display the list of emails.
-    return dict(email_list=email_list)
+    users = get_info("get users")
+    session.users = users
+    #print users.get('results')
+    return dict(users=users)
 
-
-def user():
-    """
-    exposes:
-    http://..../[app]/default/user/login
-    http://..../[app]/default/user/logout
-    http://..../[app]/default/user/register
-    http://..../[app]/default/user/profile
-    http://..../[app]/default/user/retrieve_password
-    http://..../[app]/default/user/change_password
-    http://..../[app]/default/user/manage_users (requires membership in
-    http://..../[app]/default/user/bulk_register
-    use @auth.requires_login()
-        @auth.requires_membership('group name')
-        @auth.requires_permission('read','table name',record_id)
-    to decorate functions that need access control
-    """
-    return dict(form=auth())
