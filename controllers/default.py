d321a62928305fea249f658cee719cccb49b4782
Shayan Farmani <sfarmani@ucsc.edu>
2015-11-20 00:03:08 -0800

    added more buttons. made login btn login when pressed enter. added a time out session. populated name_list, don't have search completely working yet.

--- controllers/default.py
+++ controllers/default.py
@@ -9,42 +9,26 @@ apiKey = "4udDwLuCkvJLR09ypSp1xsgKKVwBDmncRSRBd24K"
 
 
 def get_info(strng):
-    return strng
-    # if strng == "get users":
-    #     connection = httplib.HTTPSConnection('api.parse.com', 443)
-    #     connection.connect()
-    #     connection.request('GET', '/1/users', '', {
-    #            "X-Parse-Application-Id": appId,
-    #            "X-Parse-REST-API-Key": apiKey
-    #          })
-    #     return json.loads(connection.getresponse().read())
-    # elif strng == "signup as tutor":
-    #     connection = httplib.HTTPSConnection('api.parse.com', 443)
-    #     connection.connect()
-    #     connection.request('POST', '/1/users', json.dumps({
-    #            "username": "cooldude6",
-    #            "password": "p_n7!-e8",
-    #            "phone": "415-392-0202"
-    #          }), {
-    #            "X-Parse-Application-Id": appId,
-    #            "X-Parse-REST-API-Key": apiKey,
-    #            "X-Parse-Revocable-Session": "1",
-    #            "Content-Type": "application/json"
-    #          })
-    #     return json.loads(connection.getresponse().read())
+    if strng == "get users":
+        connection = httplib.HTTPSConnection('api.parse.com', 443)
+        connection.connect()
+        connection.request('GET', '/1/users', '', {
+               "X-Parse-Application-Id": appId,
+               "X-Parse-REST-API-Key": apiKey
+             })
+        return json.loads(connection.getresponse().read())
 
 
 def index():
-    users = get_info("get users")
-    session.users = users
-    return dict(users=users)
+    return dict()
 
 
 def name_list():
     users = get_info("get users")
     session.users = users
-
     return dict(users=users)
 
 
+def editprof():
+    return dict()
 
