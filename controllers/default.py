e1ae94e603e41004b796cfd298badbe08f22d1c2
bradcardello <bradcardello34@gmail.com>
2015-11-24 21:43:58 -0800

    Added a minimalistic flow from index to messaging, passing along most of the necessary data for messaging a specific user. Not functional yet, though.

--- controllers/default.py
+++ controllers/default.py
@@ -3,6 +3,7 @@
 
 import json
 import httplib
+import re
 
 appId = "sm3IJPOksqi4vIIN99wmppWnGWFZ0lvsVLNQ9VuO"
 apiKey = "4udDwLuCkvJLR09ypSp1xsgKKVwBDmncRSRBd24K"
@@ -15,7 +16,16 @@ def get_info(strng):
         connection.request('GET', '/1/users', '', {
                "X-Parse-Application-Id": appId,
                "X-Parse-REST-API-Key": apiKey
-             })
+        })
+        return json.loads(connection.getresponse().read())
+    elif strng == "get messages":
+        connection = httplib.HTTPSConnection('api.parse.com', 443)
+        connection.connect()
+        connection.request('GET', '/1/classes/ParseMessage', '', {
+               "X-Parse-Application-Id": appId,
+               "X-Parse-REST-API-Key": apiKey
+        })
+        # print json.loads(connection.getresponse().read())
         return json.loads(connection.getresponse().read())
 
 
@@ -31,4 +41,24 @@ def name_list():
 
 def editprof():
     return dict()
+
+
+def messaging():
+    users = get_info("get users")
+    session.users = users
+    return dict(users=users)
+
+
+def message_user():
+    # replace non-alphanumeric characters from args with spaces
+    title = request.args(0)
+    tutor_name = re.sub('[^0-9a-zA-Z]+', ' ', title)
+
+    users = get_info("get users")
+    session.users = users
+
+    messages = get_info("get messages")
+    # session.messages = messages
+    return dict(tutor_name=tutor_name, users=users, messages=messages)
+
 
---
+++ static/js/require-inline.js
@@ -0,0 +1,145 @@
+/*
+ * Require-inline
+ *
+ * Used for sync inline requires while the document is being written only.
+ *
+ * For example, useful for loading jquery inline.
+ *
+ * There is no good use case to use a sync require after the document is closed,
+ * and if this was implemented it would have to use eval, which is not worthwhile.
+ *
+ * Usage:
+ *
+ *   <script>require.inline('dep');</script>
+ *
+ * OR for CSP compatibility:
+ *
+ *   <script src="require-inline.js" data-context="_" data-require="depId"></script>
+ *   <script>
+ *     //dep is now defined along with all subdependencies
+ *     var dep = require('depId');
+ *   </script>
+ *
+ *   - Loads the dep synchronously at that point in the document.
+ *   - A subsequent inline script can rely on the fact that dep and all its
+ *     sub-dependencies will have been loaded already
+ *   - If the dependency maps to a layer, the entire layer will be downloaded and defined synchronously.
+ *   - If the dependency or a subdependency uses a loader plugin, it can check require.inlineRequire
+ *     to tell if it should be sync.
+ *     If a loader plugin doesn't run the callback synchronously the dependency won't be defined
+ *   - By default, the context is '_'
+ *
+ * To load multiple dependencies, use:
+ *
+ * <script src="require-inline.js" data-require="depId,depId2"></script>
+ *
+ * If you have any dependencies that actually need commas, simply escape the comma with a '\,'.
+ *
+ */
+
+/*
+var config = context.config.config;
+var _textXhr = config.text.createXhr;
+var _csXhr = config.cs.createXhr;
+config.text.createXhr = createXhr;
+config.cs.createXhr = createXhr;
+//do stuff
+config.text.createXhr = _textXhr;
+config.cs.createXhr = _csXhr;
+ */
+
+//make volo think this is an amd module
+if (false) define(null);
+
+(function() {
+
+  //get the script tag for this script
+  var scripts = document.getElementsByTagName('script');
+  var scriptTag = scripts[scripts.length - 1];
+
+  // override the load and nextTick methods for the context
+  // we temporarily change to a sync load putting it back after
+  // - the beauty of sync is that we can do things like this without conflict
+  // (as long as we put them back in the same process)
+  var enableSyncLoad = function(contextName) {
+    var context = requirejs.s.contexts[contextName]
+    // marker to check if this is an inline require
+    requirejs.inlineRequire = contextName;
+
+    var _contextLoad = context.load;
+    var _contextTick = context.nextTick;
+
+    context.nextTick = function(callback) { callback(); }
+
+    var lt = '<';
+
+    context.load = function(id, url) {
+      //two scripts - first is loading script, second is this script again, but with data attributes for callback triggering
+      document.write(lt + 'script type="text/javascript" src="' + url + '">' + lt + '/script>');
+      document.write(lt + 'script type="text/javascript" src="' + scriptTag.src + '" ' +
+        'data-requiremodule="' + id + '" data-requirecontext="' + contextName + '">' + lt + '/script>');
+    }
+
+    // immediately return the disable function
+    // must be run in the same cycle to revert the load and tick apis
+    return function() {
+      delete requirejs.inlineRequire;
+      context.nextTick = _contextTick;
+      context.load = _contextLoad;
+    }
+  }
+
+  // add a require.inline function call
+  if (!requirejs.inline) {
+    requirejs.inline = function(deps, context) {
+      if (typeof deps == 'string')
+        deps = [deps];
+
+      context = context || '_';
+
+      var disableSyncLoad = enableSyncLoad(context);
+      requirejs.s.contexts[context].require(deps);
+      disableSyncLoad();
+    }
+  }
+
+  // check for data-attribute indicating to do a load
+  var requireDep = scriptTag.getAttribute('data-require');
+
+  // do a load
+  if (requireDep) {
+    // split with a comma for multiple dependencies
+    var deps = requireDep.split(',');
+    // join back any items that were actually escaped (\,)
+    for (var i = 0; i < deps.length; i++)
+      if (deps[i].substr(deps[i].length - 1) == '\\') {
+        deps[i] = deps[i] + ',' + deps[i + 1];
+        deps.splice(i + 1, 1);
+      }
+
+    requirejs.inline(deps, scriptTag.getAttribute('data-context'));
+
+    //remove this script tag
+    scriptTag.parentNode.removeChild(scriptTag);
+  }
+  else {
+    // check for data-attributes indicating a load callback
+    var requireModule = scriptTag.getAttribute('data-requiremodule');
+    var requireContext = scriptTag.getAttribute('data-requirecontext');
+
+    //if so - this load is a loaded callback -> trigger
+    if (requireModule && requireContext) {
+
+      var disableSyncLoad = enableSyncLoad(requireContext);
+
+      requirejs.s.contexts[requireContext].completeLoad(requireModule);
+
+      disableSyncLoad();
+
+      //remove this script tag and the one before it used for the load
+      var prevScript = scriptTag.previousSibling;
+      prevScript.parentNode.removeChild(prevScript);
+      scriptTag.parentNode.removeChild(scriptTag);
+    }
+  }
+})();
